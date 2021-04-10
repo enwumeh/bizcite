ca47eadecba986079f2f3903f177efa7b5207ba6
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-04-10 13:29:41 -0500

    full crud backend, routes added and tested

--- db.sql
+++ db.sql
@@ -11,5 +11,8 @@ CREATE TABLE oils (
 
 INSERT INTO oils (name, companytype, price, stock) VALUES ('Good Girl', 'Carolina Herrera', 8, 7), ('Baccarat Rouge', 'Maison Francis Kurkdjian', 8, 5 ), ('Goddess', 'Baby Phat', 8, 2 ), ('Black Opium', 'YSL', 8, 6 ), ('Original', 'Jimmy Choo', 8, 6), ('Pink Friday', 'Nicki Minaj', 8, 3 );
 
+INSERT INTO oils (name, companytype, price, stock) VALUES ('Tobacco Vanille', 'Tom Ford', 8, 0);
+
+
 
 
--- server.js
+++ server.js
@@ -1,9 +1,103 @@
-require('dotenv').config();
-const express = require('express');
-
+require("dotenv").config();
+const express = require("express");
+// const morgan = require('morgan');
+const db = require("./server/db");
 const app = express();
+const bodyParser = require("body-parser");
 
-const port = process.env.PORT;
+//ALL OILS
+app.use(express.json());
+
+app.get("/api/v1/oils", async (req, res) => {
+  try {
+    const oilsQuery = await db.query("SELECT * FROM oils")
+    // console.log(oilsQuery)
+    res.status(200).json({
+  status: "success",
+      data: {
+    length: oilsQuery.rowCount,
+    oils: oilsQuery.rows
+  }
+});
+  }
+  catch (err) {
+    console.log(err)
+  }
+  
+})
+
+//ONE OIL
+app.get("/api/v1/oils/:id", async (req, res) => {
+  try {
+    const oilQuery = await db.query("SELECT * FROM oils where id = $1", [req.params.id]);
+    console.log(oilQuery)
+    res.status(200).json({
+  status: "success",
+      data: {
+    oil: oilQuery.rows
+  }
+});
+  }
+  catch (err) {
+    console.log(err)
+  }
+  
+})
+
+//CREATING AN OIL ENTRY
+app.post("/api/v1/oils", async (req, res) => {
+  try {
+    const oilQuery = await db.query("INSERT INTO oils (name, companytype, price, stock) VALUES ($1, $2, $3, $4) returning *", [req.body.name, req.body.companytype, req.body.price, req.body.stock]);
+    console.log(oilQuery)
+    res.status(201).json({
+  status: "success",
+      data: {
+    oils: req.body
+  }
+});
+  }
+  catch (err) {
+    console.log("MISTAKE WRONG",err)
+  }
+  
+})
+
+//UPDATING AN OIL ENTRY
+app.put("/api/v1/oils/:id", async (req, res) => {
+  try {
+    const oilQuery = await db.query("UPDATE oils SET name = $1, companytype = $2, price = $3, stock = $4 WHERE id = $5 returning *", [req.body.name, req.body.companytype, req.body.price, req.body.stock, req.params.id]);
+    console.log(oilQuery)
+    res.status(200).json({
+  status: "success",
+      data: {
+    oil: oilQuery.rows
+  }
+});
+  }
+  catch (err) {
+    console.log(err)
+  }
+  
+})
+
+//DELETING AN OIL ENTRY
+app.delete("/api/v1/oils/:id", async (req, res) => {
+
+  try {
+    const DELETED = await db.query("DELETE FROM oils WHERE id = $1 returning *", [req.params.id]);
+    // console.log(DELETED)
+    res.status(204).json({
+      status: "success",
+      data: DELETED
+    })
+  }
+  catch (err) {
+    console.log(err)
+  }
+});
+
+
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
-  console.log(`server is live! listening on port ${port}`)
+  console.log(`server is live! listening on port ${port}`);
 });
