c2b91e18d55343a13c93daba28aa6af23c991b57
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-04-09 19:31:26 -0500

    server js and directories and database created

---
+++ db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE oilsite
+
+CREATE TABLE oils (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(50) NOT NULL,
+  companytype VARCHAR(50) NOT NULL,
+  price NUMERIC NOT NULL,
+  stock INT NOT NULL
+);
+
+
+INSERT INTO oils (name, companytype, price, stock) VALUES ('Good Girl', 'Carolina Herrera', 8, 7), ('Baccarat Rouge', 'Maison Francis Kurkdjian', 8, 5 ), ('Goddess', 'Baby Phat', 8, 2 ), ('Black Opium', 'YSL', 8, 6 ), ('Original', 'Jimmy Choo', 8, 6), ('Pink Friday', 'Nicki Minaj', 8, 3 );
+
+
+
