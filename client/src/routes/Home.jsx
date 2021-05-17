753e4f081552a900f388cac60c1c6fa299a0219f
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-05-17 08:50:23 -0500

    added link to oils

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from '../styles/scss/home.scss'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
   return (
     <div className="home-main">
+      <Link to="/oils">
+        <button>See all fragrances</button>
+        </Link>
       Home page
     </div>
   )
