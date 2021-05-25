f68e5b1aa81cf364d29b10c1ff9cea926c598520
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-05-25 17:07:43 -0500

    about blerb

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,44 +1,30 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Link } from 'react-router-dom';
-import styles from '../styles/scss/nav-bar.scss';
-
+import { Link } from "react-router-dom";
+import styles from "../styles/scss/nav-bar.scss";
 
 const Nav = () => {
-  
-
-
-
   return (
     <div className="nav-div">
       <ul className="big-list">
         <li className="list">
-          <Link
-            className="Link"
-            to="/home"
-          >
+          <Link className="Link" to="/home">
             Home
           </Link>
         </li>
         <li className="list">
-          <Link
-            className="Link"
-            to="/about"
-          >
+          <Link className="Link" to="/about">
             About
           </Link>
         </li>
 
         <li className="list">
-          <Link
-            className="Link"
-            to="/oils"
-          >
+          <Link className="Link" to="/oils">
             Shop
           </Link>
         </li>
       </ul>
-      </div>
+    </div>
   );
 };
 
--- client/src/routes/About.jsx
+++ client/src/routes/About.jsx
@@ -3,29 +3,43 @@ import styles from "../styles/scss/about.scss";
 import Layout from "../components/Layout";
 import { Link } from "react-router-dom";
 
-
 const About = () => {
   return (
     <Layout>
-    <div className="about-div">
-      <p className="about-p">
-        It all started when I was gifted Pink Friday by Nicki Minaj Perfume and
-        was OBSESSED. When it finished (shortly after), I looked for more and
-        stumbled across an inexpensive body oil that claimed to smell just like
-        Pink Friday. I was surprised to find out how similar it was, and how
-        much longer it lasted. And I got compliments constantly! Since then I've
-        sworn by oil fragrances, and want to spread the love
-        <br />
-        <br />
-        All fragrances are hand filled, and made with tried and true, name brand
-        fragrances in mind. Smell amazing all day for just a fraction of the
-        price. Find your new signature scent!
-      </p>
-      <Link to="/oils">
-      <button className="home-button">See all fragrances</button>
+      <div className="about-div">
+        <p className="about-p">
+          It all started when I was gifted Pink Friday by Nicki Minaj Perfume
+          and was OBSESSED. When it finished (shortly after), I looked for more
+          and stumbled across an inexpensive body oil that claimed to smell just
+          like Pink Friday. I was surprised to find out how similar it was, and
+          how much longer it lasted. And I got compliments constantly! Since
+          then I've sworn by oil fragrances, and want to spread the love
+          <br />
+          <br />
+          All fragrances are hand filled, and made with tried and true, name
+          brand fragrances in mind. Smell amazing all day for just a fraction of
+          the price. Find your new signature scent!
+          <br />
+          <br />
+          The fragrances are made up of essential oils, synthetic aromatic
+          compounds, natural isolates and a carrier oil (DPG) to blend
+          everything components together and make the fragrance wearable. The
+          manufacturing process is regulated by the IFRA (International
+          Fragrance Association) so the fragrances are hypoallergenic (more info
+          on the IFRA 
+          <a
+            id="ifra-link"
+            href="https://ifrafragrance.org/safe-use/introduction"
+          >
+            {" "}here
+          </a>
+        </p>
+
+        <Link to="/oils">
+          <button className="home-button">See all fragrances</button>
         </Link>
       </div>
-      </Layout>
+    </Layout>
   );
 };
 
