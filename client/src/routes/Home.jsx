dfe2d2e148f592b2d816105ff421ae936e92f2a6
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-06-21 23:24:07 -0500

    changed layout colors

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useEffect, useContext } from "react";
 import styles from '../styles/scss/home.scss'
+import { OilsContext } from "../context/OilsContext";
 import { Link } from 'react-router-dom';
 import Layout from "../components/Layout";
 
 
 const Home = () => {
+  const { oils, setOils } = useContext(OilsContext);
+setOils(oils)
   return (
     <Layout>
     <div className="home-main">
-        
+       
       </div>
       <Link to="/oils">
+
       <button id="home-button">See all fragrances</button>
         </Link>
       </Layout>
