e72de3738efba733603006288727871aa365e080
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-06-28 10:59:22 -0500

    put server into server directory

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 // const morgan = require('morgan');
-const db = require("./server/db/index.js");
+const db = require("./db/index.js");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
