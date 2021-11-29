66c6af6d80cd7beb4a93209086ea41b3d6187402
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-11-29 11:25:53 -0600

    added adds ons

--- db/index.js
+++ db/index.js
@@ -26,7 +26,8 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; //heroku addon
+const proConfig = process.env.DATABASE_URL;
+console.log("PROCONFIG:", proConfig, devConfig) //heroku addon
 
 const pool = new Pool({
     connectionString:
