fd5105341d222093f845210a4f533e5ab52cc6b5
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-12-20 11:23:15 -0600

    added conditional to index.js

--- db/index.js
+++ db/index.js
@@ -29,9 +29,12 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 const proConfig = process.env.DATABASE_URL;
 console.log("ProConfig ====>", proConfig, devConfig) //heroku addon
 
+if (process.env.NODE_ENV === 'development') {
+  ssl = {rejectUnauthorized: false};
+}
 const pool = new Pool({
   connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
-  ssl: { rejectUnauthorized: false }
+  // ssl: { rejectUnauthorized: false }
 
     // process.env.NODE_ENV === 'production' ? devConfig :  proConfig
   
