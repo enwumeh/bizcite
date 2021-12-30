f80bd18eb8e99d13dc4610c4864c063a02b9a01a
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-12-30 09:03:54 -0600

    changed imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,48 @@
 import React from "react";
 import Home from "./routes/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import Oildetails from "./routes/Oildetails";
 import About from "./routes/About";
 import Oils from "./components/Oils";
 import { OilsContextProvider } from "./context/OilsContext";
 import LandingPage from "./routes/LandingPage";
 
-
-
 const App = () => {
   return (
     <OilsContextProvider>
-      <div className="container">
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<LandingPage/>}/>
-            {/* <LandingPage /> */}
-            <Route exact path="/home" element={<Home />} />
-            <Route exact path="/about" element={<About />}/>
-            <Route exact path="/oils" element={<Oils />} />
-            <Route exact path="/oils/:id" element={<Oildetails />}/>
-          </Routes>
-        </Router>
-      </div>
+      {/* <Router> */}
+      <Route>
+        <div className="container">
+          <Route exact path="/" element={<LandingPage />} />
+          {/* <LandingPage /> */}
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/oils" element={<Oils />} />
+          <Route exact path="/oils/:id" element={<Oildetails />} />
+        </div>
+      </Route>
+      {/* </Router> */}
     </OilsContextProvider>
   );
 };
+
+// const App = () => {
+//   return (
+//     <OilsContextProvider>
+//       <div className="container">
+//         <Router>
+//           <Routes>
+//             <Route exact path="/" element={<LandingPage/>}/>
+//             {/* <LandingPage /> */}
+//             <Route exact path="/home" element={<Home />} />
+//             <Route exact path="/about" element={<About />}/>
+//             <Route exact path="/oils" element={<Oils />} />
+//             <Route exact path="/oils/:id" element={<Oildetails />}/>
+//           </Routes>
+//         </Router>
+//       </div>
+//     </OilsContextProvider>
+//   );
+// };
 
 export default App;
